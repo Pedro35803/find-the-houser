bfcc0b592d383f8467172f9145eb793376db9d57
Pedro Júnior <pedrojuniorifpb@gmail.com>
2022-10-04 10:25:29 -0300

    Adicionando script que troca apenas os textos do card main e dos cards menores

---
+++ js/trocar_comentario.js
@@ -0,0 +1,24 @@
+import comentarios from './comentarios.json' assert { type: "json" }; 
+
+const comment = document.querySelectorAll(".card-testimonies")
+
+comment.forEach(element => element.addEventListener("click", () => {
+    const nameElement = element.querySelector(".profile-name")
+    const nameMain = document.querySelector(".card-main  .profile-name-main")
+
+    const elementJson = comentarios.find(item => {
+        return item.name == nameElement.textContent
+    });
+
+    nameElement.textContent = nameMain.textContent;
+    nameMain.textContent = elementJson.name;
+
+    const photoElement = element.querySelector(".profile-image")
+    const photoMain = document.querySelector(".card-main  .profile-image-main")
+
+    photoElement.src = photoMain.src;
+    photoMain.src = elementJson.urlImage;
+
+    const commentMain = document.querySelector(".card-main  .commentary-text")
+    commentMain.textContent = elementJson.textCommentary
+}))
